5c49531b59d0db4d6f5923b9258624c94f0244f9
Deen <sumailasherif773@gmail.com>
2026-04-27 20:59:12 +0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ============================================================
+# Z-Score Calculator & Student Exam Score Analyzer
+# ============================================================
