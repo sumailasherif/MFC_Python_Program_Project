4940de1fbd690c69fea4b03a3e24d583c67e1e34
MFC_Python_Program_Project <sumailasherif773@gmail.com>
2026-05-24 16:15:04 +0400

    get user input

--- Main.py
+++ Main.py
@@ -2,4 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
+# This helps to get user input to calculate
+print("Enter numbers separated by spaces (e.g. 10 12 15 18 20):")
+user_input = input(" ")
